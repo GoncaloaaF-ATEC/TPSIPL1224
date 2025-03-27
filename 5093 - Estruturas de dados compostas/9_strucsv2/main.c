48cfd730b2ccfdd0cc60f5f7e2d8bc000b41566c
Goncalo Feliciano <goncaloaaf@gmail.com>
2025-03-27 18:31:54 +0000

    demo strucs compostas

--- 5093 - Estruturas de dados compostas/9_strucsv2/main.c
+++ 5093 - Estruturas de dados compostas/9_strucsv2/main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /*
  *
@@ -22,31 +24,79 @@ typedef struct customer{
 
 } Customer ;
 
-typedef struct pordutos{
+typedef struct porduto{
     char mome[50];
     float preco;
-} Pordutos;
+} Porduto;
+
+typedef struct data{
+    int dia;
+    int mes;
+    int ano;
+} Data;
+
 
 typedef struct order{
-    Pordutos listaPordutos[10];
-    Customer  ct;
-    char data[8];
+    Porduto Pordutos[10];
+    Customer customer;
+    Data data;
 } Order;
 
 
 
+typedef struct morada{
+    char rua[500];
+    char porta[10];
+    char apt[10];
+
+} Morada;
 
 
+typedef struct cliente{
+    char nome[50];
+    Morada morada;
+} Cliente;
 
 
-
-
-
-
-
-
+void removerChar(char str[], char c[]);
 
 int main(void) {
-    printf("Hello, World!\n");
+
+    Cliente clt;
+
+    printf("nome: ");
+    fgets(clt.nome, sizeof(clt.nome), stdin);
+    removerChar(clt.nome, "\n");
+
+    printf("Morada\n");
+    printf("Rua: ");
+    fgets(clt.morada.rua, sizeof(clt.morada.rua), stdin);
+
+    removerChar(clt.morada.rua, "\n");
+    
+    printf("porta: ");
+    fgets(clt.morada.porta, sizeof(clt.morada.porta), stdin);
+    removerChar(clt.morada.porta, "\n");
+
+
+    printf("apt: ");
+    fgets(clt.morada.apt, sizeof(clt.morada.apt), stdin);
+    removerChar(clt.morada.apt, "\n");
+
+
+    printf("O nome é %s\n", clt.nome);
+    printf("O rua é %s\n", clt.morada.rua);
+    printf("O porta é %s\n", clt.morada.porta);
+    printf("O apt é %s\n", clt.morada.apt);
+
     return 0;
+}
+
+void removerChar(char str[], char c[]){
+
+    int i = strcspn(str, c);
+    str[i] = '\0';
+    fflush(stdin);
+
+
 }
